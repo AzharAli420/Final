a69a96f48514642eea03e1de720c84b6493d690f
Azhar Ali <l1s21bsse0062@ucp.edu.pk>
2024-02-01 12:23:24 +0500

    Added Feature

--- 2-learning.cpp
+++ 2-learning.cpp
@@ -1,16 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class learning
+class e-learning
 {
-	private:
-	int id;
-	string name;
-	public:
-	void setId();
-	void setName();
-	int getId();
-	string getName();
-};
-int main()
-	
-}
+      private:
+              int id;
+              string name;
+      public:
+      void setId(int id)
+      { id=id;
+           }
+      void setName(string name)
+      {
+           name=name;
+           }
+      int getId()
+      {
+          return id;
+          }
+      string getName()
+      {
+             return name;
+             }
+      void setName();
+
+      int getId();
+
+      int getId()
+      {
+          return id;
+          }
+      string getName();
+      };
+      int main()
+      {
+          }
