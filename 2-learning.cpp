6864d2342c6a7b7416e24457890909ef641f2192
Azhar Ali <l1s21bsse0062@ucp.edu.pk>
2024-02-01 11:51:42 +0500

    Added Files

---
+++ 2-learning.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+class learning
+{
+	private:
+	int id;
+	string name;
+	public:
+	void setId();
+	void setName();
+	int getId();
+	string getName();
+};
+int main()
+	
+}
